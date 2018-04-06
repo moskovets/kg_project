044c316a3763ab2d162548494425542f283d5594
Zotkin Alexander <alexanderzot@gmail.com>
2018-04-07 01:15:53 +0300

    Add flat draw fractal

--- CImage.cpp
+++ CImage.cpp
@@ -92,167 +92,8 @@ void CImage::changeScale(tScene &scene, int sizepixel)
 
 void CImage::algo(tScene &scene, tPaintParam &param, BaseFunction *func, tParamFractal &paramFract)
 {
-/*    std::ofstream out;
-    out.open("sphere.obj");
-    {
-        Mesh* mesh = new Mesh();
-        mesh->addSphere(20, 20, 3);
-        mesh->operator <<(out);
-    }
-    out.close();
-    std::cout << "ok" << std::endl;
-//    return;
-
-*/
-
-    qDebug() << image.height() << image.width();
-/*
-      {
-        SetDrawer setDrawer(image.height(), image.width(), 20, 20, 0.5);
-        setDrawer.setPixel(Vector4(0, 0, 2), Color(0, 255));
-        setDrawer.setPixel(Vector4(2, 3, 20), Color(0, 255));
-        setDrawer.setPixel(Vector4(1, 1, 15), Color(0, 255));
-        setDrawer.setPixel(Vector4(-2, -3, 15), Color(0, 255));
-        setDrawer.setPixel(Vector4(5, -10, 15), Color(0, 255));
-        setDrawer.setPixel(Vector4(-2, 3, 15), Color(255));
-        setDrawer.setPixel(Vector4(-2, 8, 15), Color(255, 255));
-
-        image = setDrawer.getImage().scaled(image.width(), image.height());
-        printOnScene(scene);
-    }
-    //вывести сферу, перенести ее по z только (модель) на 4 единицы
-    //Сферу лучше с большим кол-вом разбиений создавать - 50 - 150
-    //Радиус 1
-
-return;
-/*
-    {
-        std::shared_ptr<FrameBuffer> frame(new FrameBuffer(image.height(), image.width()));
-
-        Drawer3D drawer(frame, DME_FLAT);
-        Render render(frame);
-        {
-            Mesh* mesh = new Mesh();
-
-            mesh->addSphere(20, 20, 3);
-            Model model(mesh, Color(0, 255), Vector4(0, 0, 8));
-            drawer.drawModel(model);
-
-        }
-        {
-            Mesh* mesh = new Mesh();
-            Vector4 vertex[3] = { Vector4(0, 0, 1),
-                                  Vector4(0.1, 0, 1),
-                                  Vector4(0, 1, 1)
-                                };
-            Vector4 normal[3] = { Vector4(0, 0, -1),
-                                  Vector4(0, 0, -1),
-                                  Vector4(0, 0, -1)
-                                };
-            Triangle triangle(vertex, normal);
-            mesh->addTriangle(triangle);
-            Model model(mesh, Color(255), Vector4(0, 0, 0));
-            drawer.drawModel(model);
-
-        }
-        /* {
-        Mesh* mesh = new Mesh();
-        Vector4 vertex[3] = { Vector4(-0.05, -0.2, 3),
-                              Vector4(0.05, -0.2, 3),
-                              Vector4(0, 0.15, 1)
-                            };
-        Vector4 normal[3] = { Vector4(0, 0, -1),
-                              Vector4(0, 0, -1),
-                              Vector4(0, 0, -1)
-                            };
-        Triangle triangle(vertex, normal);
-        mesh->addTriangle(triangle);
-        Model model(mesh, Color(255), Vector4(0, 0, 5));
-        //    Model model(mesh, Color(0, 255), Vector4(0.2, 0.2, 0));
-        //    Model model(mesh, Color(0, 255));
-        drawer.drawModel(model);
-    }
-        drawer.swap();
-        image = render.getImage().scaled(image.width(), image.height());
-        printOnScene(scene);
-    }
-    return;
-/*    Dimension4 d4;
-    QColor c;
-    for(double i = 0; i < 400; i++) {
-        if(i == 150) {
-            int j = 5;
-        }
-        c = d4.getColorByLen(i + 380);
-        for(int j = 0; j < 30; j++) {
-            addPixel(tPoint(i/2, j + 100), c);
-//            if(i > 200)
-//                addPixel(tPoint(i - 200, j + 50), c);
-//            else
-//                addPixel(tPoint(i, j + 100), c);
-        }
-    }
-    printOnScene(scene);
-    return;
-*/
-    BaseFunction *f = func; //new Function1(Quaternion(-0.65, -0.5)); //, 0.2, 0.3, 0.4));
-
-
-   const double r = 3;
-
-//    BaseFunction *f = new Function2(Quaternion(0.19, -0.5, 0, 0.1)); //, 0.2, 0.3, 0.4));
-    //BaseFunction *f = new Function1(Quaternion(-0.65, -0.5)); //, 0.2, 0.3, 0.4));
-   // JuliaSet set(f, r, 15);
-
-   JuliaSet set(f, paramFract.r, paramFract.maxIter);
-
+    JuliaSet set(func, paramFract.r, paramFract.maxIter);
     JuliaSetAlgo algoSet(&set);
-
-/*
-    double x = 0.15;
-    double y = 0;
-
-    double wmin = -r;
-    double wmax = r;
-    double dw = (wmax - wmin) / image.width();
-
-    double w = wmin;
-    double z;
-    double zStart = -r;
-    for (int wScreen = 0; wScreen < image.width(); wScreen++) {
-        Quaternion startQ(x, y, zStart, w);
-        Quaternion res;
-        if (algoSet.findMinSolutionByC(startQ, r, res)) {
-            z = res.c();
-            addPixel(tPoint(wScreen, z * image.height() / 2 / r + image.height() / 2), param.color);
-        }
-        else {
-            z = res.c();
-            addPixel(tPoint(wScreen, z * image.height() / 2 / r + image.height() / 2), Qt::red);
-        }
-        qDebug() << w << "\t" << z;
-
-        w += dw;
-    }
-
-*/
-    /*
-    double xmin = -2;
-    double xmax = 2;
-
-    double ymin = -1;
-    double ymax = 1;
-
-    double dx = (xmax - xmin) / image.width();
-    double dy = (ymax - ymin) / image.height();
-
-    double w = 0;
-    double z;
-
-    double zmin = -2;
-    double zmax = 2;
-
-*/
 
     double xmin = paramFract.xmin; //-2;
     double xmax = paramFract.xmax; //2;
@@ -260,77 +101,55 @@ return;
     double ymin = paramFract.ymin; //-1;
     double ymax = paramFract.ymax; //1;
 
-    double w = 0;
-    double z;
-
     double zmin = paramFract.zmin; //-2;
     double zmax = paramFract.zmax; //2;
 
-    double height = image.height() / 4;
-    double width = image.width() / 4;
-
-    SetDrawer setDrawer(height, width, 20, 20, 0.01);
-
-    double dx = (xmax - xmin) / width;
-    double dy = (ymax - ymin) / height;
-
-    QTime timeStart = QTime::currentTime();
+    double height = image.height();
+    double width = image.width();
 
 
-    for (double x = xmin; x < xmax; x += dx) {
-        for (double y = ymin; y < ymax; y += dy) {
-            Quaternion startQ(x, y, zmin, w);
-            Quaternion res;
-            if (algoSet.findMinSolutionByC(startQ, zmax, res)) {
-                z = res.c();
+    std::shared_ptr<FrameBuffer> fbuf(new FrameBuffer(image.height(), image.width()));
 
-                double xDrawer = 2 * (x - xmin) / (xmax - xmin) - 1;
-                double yDrawer = 2 * (y - ymin) / (ymax - ymin) - 1;
-                double zDrawer = z - zmin + 0.2;
+    Quaternion res;
+    double depth[image.height() + 2][image.width() + 2];
 
-                Vector4 pos(xDrawer, yDrawer, zDrawer);
-                setDrawer.setPixel(pos, Color(255));
-            }
+    for (int y = 0; y < image.height() + 2; ++y)
+    {
+        double curY = (((2 * double(y) / (height + 2) - 1.0) + 1) * (ymax - ymin) / 2);
+        for (int x = 0; x < image.width() + 2; ++x)
+        {
+            double curX = (((2 * double(x) / (width + 2) - 1.0) + 1) * (xmax - xmin) / 2);
+            Quaternion startQ(curX, curY, zmin, 0);
+            if (algoSet.findMinSolutionByC(startQ, zmax, res))
+                depth[y][x] = res.c();
+            else
+                depth[y][x] = 1e10; // TODO big
         }
     }
 
-    QTime timePreRender = QTime::currentTime();
+    Vector4 light(1/sqrt(3), 1/sqrt(3), 1/sqrt(3));
+    light = -light;
 
+    for (int y = 1; y < image.height() + 1; ++y)
+    {
+        for (int x = 1; x < image.width() + 1; ++x)
+        {
+            Vector4 norm(
+                            (depth[y][x + 1] - depth[y][x - 1]) * width / (xmax - xmin),
+                            (depth[y + 1][x] - depth[y - 1][x]) * height / (ymax - ymin),
+                            2
+                        );
+            norm.normalize3();
 
-/*    {
-        double x = xmin;
-        double dx = (xmax - xmin) / image.width();
-        double dy = (ymax - ymin) / image.height();
-
-        for (int xScreen = 0; xScreen < image.width(); xScreen++) {
-            double y = ymin;
-            for (int yScreen = 0; yScreen < image.height(); yScreen++) {
-                Quaternion startQ(x, y, zmin, w);
-                Quaternion res;
-
-                if (algoSet.findMinSolutionByC(startQ, zmax, res)) {
-                    z = res.c();
-                    addPixel(tPoint(xScreen, yScreen, (z - zmin) / (zmax - zmin)), param.color);
-
-                    double xDrawer = round((x/width + 1) * (height / 2.0));;
-                    double yDrawer = round((y/width + 1) * (height / 2.0));
-                    double zDrawer = z;
-
-                    Vector4 pos(xDrawer, yDrawer, zDrawer, z);
-                    setDrawer.setPixel(pos, Color(param.color.red(), param.color.green(), param.color.blue(), param.color.alpha()));
-                }
-                y += dy;
-            }
-            x += dx;
+            double I = light * norm;
+            Color c(255 * I);
+            fbuf->getBuffer()->addPixel(x - 1, y - 1, depth[y - 1][x - 1], c);
         }
-    }*/
-    image = setDrawer.getImage().scaled(image.width(), image.height());
-    QTime timeEnd = QTime::currentTime();
+    }
 
-std::cout << "общее время " << timeStart.msecsTo(timeEnd) << std::endl;
-std::cout << "время до рендеринга " << timeStart.msecsTo(timePreRender) << std::endl;
-
-printOnScene(scene);
+    Render render(fbuf);
+    image = render.getImage().scaled(image.width(), image.height());
+    printOnScene(scene);
 }
 
 #define THREAD_MATH_NUMBER   3
